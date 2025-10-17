1688563bd4d4df48e21cd6fe7fe388c33b0fad3d
jeremy beaussart <jeremy.beaussart@gmail.com>
2025-10-17 15:33:17 +0200

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
 export default defineConfig({
+  server: {
+    host: true, // optional, but useful if you hit the site from another device
+    port: 4321,
+  },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      hmr: {
+        host: "localhost",
+        protocol: "ws",
+        clientPort: 4321, // make the browser connect back on the HTTP port
+      },
+    },
   },
 
-  adapter: netlify()
+  adapter: netlify(),
 });
